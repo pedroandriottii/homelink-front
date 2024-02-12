8a16afbc14d2bc7e53a4111b51697be75a54341b
Pedro Andriotti <phab@cesar.school>
2024-02-12 17:53:55 -0300

    Starting homepage

--- js/navbar.js
+++ js/navbar.js
@@ -2,3 +2,21 @@ document.getElementById('hamburger-icon').addEventListener('click', function () 
   this.classList.toggle('active');
   document.getElementById('navbar-links').classList.toggle('active');
 });
+
+const counterNumber = document.getElementById("counterNumber");
+const yearsCounter = document.getElementById("yearsCounter");
+const targetNumber = 125;
+const targetYear = 6;
+let currentNumber = 0;
+
+const incrementCounter = () => {
+  if (currentNumber < targetNumber) {
+    currentNumber++;
+    counterNumber.textContent = currentNumber;
+  }
+  if (currentNumber < targetYear) {
+    yearsCounter.textContent = currentNumber;
+  }
+};
+
+setInterval(incrementCounter, 50);
